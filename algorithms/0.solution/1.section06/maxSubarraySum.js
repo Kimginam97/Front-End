c8c76a4d49d643c8a9247eea5cd47825fe1e0c70
Kimginam97 <rlarlska79@naver.com>
2023-10-05 21:13:21 +0900

    Sliding Window - maxSubarraySum minSubArrayLen findLongestSubstring

---
+++ algorithms/0.solution/1.section06/maxSubarraySum.js
@@ -0,0 +1,19 @@
+function maxSubarraySum(arr, num) {
+    let tempSum = 0
+    let maxSum = 0
+
+    if (arr.length < num) return null
+
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+
+    tempSum = maxSum
+
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(tempSum, maxSum)
+    }
+
+    return maxSum
+}
