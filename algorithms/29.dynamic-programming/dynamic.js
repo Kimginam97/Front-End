e59e8818d35ee325be915107bb3e70910d72f376
Kimginam97 <rlarlska79@naver.com>
2023-12-02 14:31:22 +0900

    table 추가

--- algorithms/29.dynamic-programming/dynamic.js
+++ algorithms/29.dynamic-programming/dynamic.js
@@ -3,10 +3,19 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2)
 }
 
-function fib2(n, memo = []) {
+function fib_memo(n, memo = []) {
     if (memo[n] !== undefined) return memo[n]
     if (n <= 2) return 1
     var res = fib(n - 1, memo) + fib(n - 2, memo)
     memo[n] = res
     return res
+}
+
+function fib_table(n) {
+    if (n <= 2) return 1
+    var fibNums = [0, 1, 1]
+    for (var i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2]
+    }
+    return fibNums[n]
 }
